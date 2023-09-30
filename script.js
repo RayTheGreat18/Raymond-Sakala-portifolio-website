afade252aedb84cb918d521d56ca255c453e3c17
Ray Sakala <103606571+RayTheGreat18@users.noreply.github.com>
2023-09-30 15:55:49 +0200

    Add card to link to js

--- script.js
+++ script.js
@@ -1,6 +1,19 @@
 const darkLightTheme = document.getElementById("dark-light-theme")
 const body = document.querySelector(".body")
+const card1 = document.querySelector(".card-one")
+const card2 = document.querySelector(".card-two")
+const card3 = document.querySelector(".card-three")
+const card4 = document.querySelector(".card-four")
+const card5 = document.querySelector(".card-five")
+const card6 = document.querySelector(".card-six")
 
 darkLightTheme.addEventListener("click", ()=>{
     body.classList.toggle("dark")
+    card1.classList.toggle("greyed")
+    card2.classList.toggle("greyed")
+    card3.classList.toggle("greyed")
+    card4.classList.toggle("greyed")
+    card5.classList.toggle("greyed")
+    card6.classList.toggle("greyed")
 })
+
